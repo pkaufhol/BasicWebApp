f7e3df3f60dc9cd4dc90536e24566f6ec3ae3fd7
Philip Kaufholz <pkaufhol@andrew.cmu.edu>
2022-09-22 09:58:57 -0400

    adding cat query to app

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,6 +8,15 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("cat")) {
+            return "The cat (Felis catus) is a domestic species of small carnivorous mammal. " +
+            "It is the only domesticated species in the family Felidae and is often referred to as the " +
+            "domestic cat or house cat to distinguish it from the wild members of the family. A cat" +
+            "can either be a house cat, a farm cat, or a feral cat; the latter ranges freely and avoids" +
+            "human contact. Domestic cats are valued by humans for companionship and their ability to" +
+            "kill rodents. About 60 cat breeds are recognized by various cat registries.";
+        }
         return "";
+
     }
 }
